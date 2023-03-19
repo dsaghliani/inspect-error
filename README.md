088f96bcbc825a59eb5c71ae24e86cc34cd6a7c7
Demetre Saghliani <saghliani.d@gmail.com>
2023-03-19 15:55:10 +0400

    Create a README & document the crate.

---
+++ README.md
@@ -0,0 +1,41 @@
+# InspectError
+
+A handy extension trait for `Result` that brings `Result::inspect_err` to stable Rust in the form of `InspectError::inspect_error`.
+
+Instead of this...
+
+```rs
+// ...
+    .map_err(|err| {
+        eprintln!("Something went wrong: '{err}'.");
+        err
+    })
+// ...
+```
+
+...write this:
+
+```rs
+// ...
+    .inspect_error(|err| eprintln!("Something went wrong: '{err}'."))
+// ...
+```
+
+# Usage
+
+Bring the `InspectError` trait into scope. This will let you call `.inspect_error()` on a `Result`.
+
+
+```rs
+use inspect_error::InspectError;
+
+fn read_magic_number_from_db() -> Result<i32, &'static str> {
+    // Pretend this is a real `Result`.
+    let magic_number = Err("couldn't connect to the database")
+        .inspect_error(|err| eprintln!("Something went wrong: '{err}'."))?;
+    Ok(magic_number)
+}
+
+let output = read_magic_number_from_db();
+assert_eq!(output, Err("couldn't connect to the database"));
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
+//! A handy extension trait for `Result` that brings `Result::inspect_err` to stable Rust in the form of `InspectError::inspect_error`.
+//!
+//! Instead of this...
+//!
+//! ```rs
+//! // ...
+//!     .map_err(|err| {
+//!         eprintln!("Something went wrong: '{err}'.");
+//!         err
+//!     })
+//! // ...
+//! ```
+//!
+//! ...write this:
+//!
+//! ```rs
+//! // ...
+//!     .inspect_error(|err| eprintln!("Something went wrong: '{err}'."))
+//! // ...
+//! ```
+//!
+//! # Usage
+//!
+//! Bring the `InspectError` trait into scope. This will let you call `.inspect_error()` on a `Result`.
+//!
+//!
+//! ```rs
+//! use inspect_error::InspectError;
+//!
+//! fn read_magic_number_from_db() -> Result<i32, &'static str> {
+//!     // Pretend this is a real `Result`.
+//!     let magic_number = Err("couldn't connect to the database")
+//!         .inspect_error(|err| eprintln!("Something went wrong: '{err}'."))?;
+//!     Ok(magic_number)
+//! }
+//!
+//! let output = read_magic_number_from_db();
+//! assert_eq!(output, Err("couldn't connect to the database"));
+//! ```
+
 pub trait InspectError<E> {
     #[must_use]
     fn inspect_error(self, inspect: impl FnOnce(&E)) -> Self;
